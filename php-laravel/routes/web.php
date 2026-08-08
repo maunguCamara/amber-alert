38e923451eebb13840bcac481fe47f723941da81
Aramrac <camaramaungu@gmail.com>
2026-08-08 21:59:06 +0300

    MOve files to fix routing. Routes hitting default Laravel route returning view(welcome). Created view as a redirect to our map

--- php-laravel/routes/web.php
+++ php-laravel/routes/web.php
@@ -1,7 +1,14 @@
 <?php
-
+use App\Http\Controllers\CaseController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CaseController::class, 'map'])->name('home');
+Route::get('/report', [CaseController::class, 'create'])->name('cases.create');
+Route::post('/report', [CaseController::class, 'store'])->name('cases.store');
+Route::get('/cases/{id}', [CaseController::class, 'show'])->name('cases.show');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
